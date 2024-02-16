e8871e23df1138f57798b97faa3f064d01802142
Tiya Noonia <tiyanoonia2901@gmail.com>
2024-02-16 18:19:04 +0000

    my code

---
+++ assignment operator.py
@@ -0,0 +1,16 @@
+# assingment operator
+a = 5
+b = 2
+c = 0
+
+c = a+b
+print(f"a+b:  {c}")
+
+c += a
+print(f"c += a: {c}")
+
+c *= a
+print(f"c *= a: {c}")
+
+c /= a
+print(f"c /= a: {c}")
---
+++ main.py
@@ -0,0 +1,27 @@
+#bitwise operator
+a = 7
+b = 6
+c = 5
+
+c = a&b
+print (f" line number 1 is {c}")
+
+c = a|b
+print (f"line number 2 is {c}")
+
+c = ~a^b
+
+print (f"line number 3 is {c}")
+
+c = ~a
+
+print (f"line number 4 {c}")
+
+c = a>>b
+
+print (f"line number 5 {c}")
+
+c = a<<b
+
+print (f"line number 6 {c}")
+
